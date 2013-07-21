f3bf3ec848d53933e517af63ad1cbe40730ee4e0
SchleicherKlaus <klaus.schleicher@gmail.com>
2013-07-21 20:08:51 +0200

    Init

---
+++ KKeyPath/KKeyPath/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  KKeyPath
+//
+//  Created by Klaus Schleicher on 21.07.13.
+//  Copyright (c) 2013 Klaus Schleicher. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
